88fc1b26edefcf3d10f4ea20de2d1e3edea596fd
ffrmns <fikrifa0@gmail.com>
2022-03-26 02:53:00 +0700

    Provide empty file with its directory, imported by server.js, without .env file

---
+++ src/server.js
@@ -0,0 +1,44 @@
+require('dotenv').config();
+
+const Hapi = require('@hapi/hapi');
+const albums = require('./api/albums');
+const songs = require('./api/songs');
+const AlbumsService = require('./services/postgres/AlbumsService');
+const SongsService = require('./services/postgres/SongsService');
+const AlbumsValidator = require('./validator/albums');
+const SongsValidator = require('./validator/songs');
+
+const init = async () => {
+  const albumsService = new AlbumsService();
+  const songsService = new SongsService();
+  const server = Hapi.server({
+    port: process.env.PORT,
+    host: process.env.HOST,
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+
+  await server.register({
+    plugin: albums,
+    options: {
+      service: albumsService,
+      validator: AlbumsValidator,
+    },
+  });
+
+  await server.register({
+    plugin: songs,
+    options: {
+      service: songsService,
+      validator: SongsValidator,
+    },
+  });
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
