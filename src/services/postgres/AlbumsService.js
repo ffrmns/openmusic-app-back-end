66bab903634482e76c06aa06e432d38270eef849
ffrmns <fikrifa0@gmail.com>
2022-03-27 16:28:22 +0700

    Fill AlbumsService.js and SongsService.js in src/services/postgres/

---
+++ src/services/postgres/AlbumsService.js
@@ -0,0 +1,69 @@
+const { Pool } = require('pg');
+const { nanoid } = require('nanoid');
+const InvariantError = require('../../exceptions/InvariantError');
+const { mapDBToModel } = require('../../utils');
+const NotFoundError = require('../../exceptions/NotFoundError');
+
+class AlbumsService {
+  constructor() {
+    this._pool = new Pool();
+  }
+
+  async addAlbum({ name, year }) {
+    const id = nanoid(16);
+
+    const query = {
+      text: 'INSERT INTO albums VALUES($1, $2, $3) RETURNING id',
+      values: [id, name],
+    };
+    const result = await this._pool.query(query);
+    if (!result.rows[0].id) {
+      throw new InvariantError('Album gagal ditambahkan.');
+    }
+
+    return result.rows[0].id;
+  }
+
+  async getAlbums() {
+    const result = await this._pool.query('SELECT * FROM albums');
+    return result.rows.map(mapDBToModel);
+  }
+
+  async getAlbumById(id) {
+    const query = {
+      text: 'SELECT * FROM albums WHERE id = $1',
+      values: [id],
+    };
+    const result = await this._pool.query(query);;
+    if (!result.rows.length) {
+      throw new NotFoundError('Album tidak ditemukan.');
+    };
+    return result.rows.map(mapDBToModel)[0];
+  }
+
+  async editAlbumById(id, { name, year }) {
+    const query = {
+      text: 'UPDATE albums SET name = $1, year = $2 WHERE id = $3 RETURNING id',
+      values: [name, year, id],
+    };
+    const result = await this._pool.query(query);
+    if (!result.rows.length) {
+      throw new NotFoundError('Gagal memperbarui album, id tidak ditemukan.');
+    }
+  }
+
+  async deleteAlbumById(id) {
+    const query = {
+      text: 'DELETE FROM albums WHERE id = $1 RETURNING id',
+      values: [id],
+    };
+
+    const result = await this._pool.query(query);
+
+    if(!result.rows.length){
+      throw new NotFoundError('Album gagal dihapus, id tidak ditemukan.');
+    }
+  }
+}
+
+module.exports = AlbumsService;
