7b72dbe9b4e131bc779e8d70beaceaa4bf43e55e
ffrmns <fikrifa0@gmail.com>
2022-03-28 17:07:56 +0700

    Fill src/utils/index.js for songs api

---
+++ src/utils/index.js
@@ -0,0 +1,19 @@
+const mapDBToModel = ({
+	id,
+	title,
+	year,
+	performer,
+	genre,
+	duration,
+	album_id,
+}) => ({
+  id,
+  title,
+  year,
+  performer,
+  genre,
+  duration,
+  albumId: album_id,
+});
+
+module.exports = { mapDBToModel };
