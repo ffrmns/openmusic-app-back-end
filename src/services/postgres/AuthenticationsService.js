756cffc627cf9aba1d1baf4824190ff51074a375
ffrmns <fikrifa0@gmail.com>
2022-04-03 23:47:03 +0700

    Create authentications service: addRefreshToken, verifyRefreshToken, deleteRefreshToken

---
+++ src/services/postgres/AuthenticationsService.js
@@ -0,0 +1,37 @@
+const { Pool } = require('pg');
+const InvariantError = require('../../exceptions/InvariantError');
+
+class AuthenticationsService {
+  constructor() {
+    this.pool = new Pool();
+  }
+
+  async addRefreshToken(token) {
+    const query = {
+      text: 'INSERT INTO authentications VALUES($1)',
+      values: [token],
+    };
+    await this.pool.query(query);
+  }
+
+  async verifyRefreshToken(token) {
+    const query = {
+      text: 'SELECT token FROM authentications WHERE token = $1',
+      values: [token],
+    };
+    const result = await this.pool.query(query);
+    if (!result.rows.length) {
+      throw new InvariantError('Refresh token tidak valid.');
+    }
+  }
+
+  async deleteRefreshToken(token) {
+    const query = {
+      text: 'DELETE FROM authentications WHERE token = $1',
+      values: [token],
+    };
+    await this.pool.query(query);
+  }
+}
+
+module.exports = AuthenticationsService;
