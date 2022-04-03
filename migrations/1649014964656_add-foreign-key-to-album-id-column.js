07c8e56ce75428c183dfd112aa2d891777cd93db
ffrmns <fikrifa0@gmail.com>
2022-04-04 03:03:25 +0700

    Add foreign key to album_id column in songs table migrations

---
+++ migrations/1649014964656_add-foreign-key-to-album-id-column.js
@@ -0,0 +1,15 @@
+/* eslint-disable camelcase */
+
+exports.shorthands = undefined;
+
+exports.up = (pgm) => {
+  pgm.sql("INSERT INTO albums(id, name, year) VALUES('no_album', 'no_album', 0)");
+  pgm.sql("UPDATE songs SET album_id = 'no_album' WHERE album_id = NULL");
+  pgm.addConstraint('songs', 'fk_songs.album_id_albums.id', 'FOREIGN KEY(album_id) REFERENCES albums(id) ON DELETE CASCADE');
+};
+
+exports.down = (pgm) => {
+  pgm.dropConstraint('songs', 'fk_songs.album_id_albums.id');
+  pgm.sql("UPDATE songs SET album_id = NULL WHERE album_id = 'no_album'");
+  pgm.sql("DELETE FROM albums WHERE id = 'no_album'");
+};
