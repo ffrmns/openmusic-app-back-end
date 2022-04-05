8c8c307ff347cce8c486d1d27779f5c48c02a46f
ffrmns <fikrifa0@gmail.com>
2022-04-05 23:59:32 +0700

    Create playlist_song_activities table migrations

---
+++ migrations/1649176733461_create-playlist-song-activities-table.js
@@ -0,0 +1,38 @@
+/* eslint-disable camelcase */
+
+exports.shorthands = undefined;
+
+exports.up = (pgm) => {
+  pgm.createTable('playlist_song_activities', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    playlist_id: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+    song_id: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+    user_id: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+    action: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    time: {
+      type: 'TEXT',
+      notNull: true,
+    },
+  });
+
+  pgm.addConstraint('playlist_song_activities', 'fk_playlist_song_activities.playlist_id_playlists.id', 'FOREIGN KEY(playlist_id) REFERENCES playlists(id) ON DELETE CASCADE');
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable('playlist_song_activities');
+};
