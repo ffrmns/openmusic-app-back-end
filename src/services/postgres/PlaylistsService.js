78c7129b2f4fccb199b1905b7ea30285edafb1b0
ffrmns <fikrifa0@gmail.com>
2022-04-08 08:54:09 +0700

    Change the verify playlist owner query to what's only needed: * to owner

--- src/services/postgres/PlaylistsService.js
+++ src/services/postgres/PlaylistsService.js
@@ -94,7 +94,7 @@ class PlaylistsService {
 
   async verifyPlaylistOwner(id, owner) {
     const query = {
-      text: 'SELECT * FROM playlists WHERE id = $1',
+      text: 'SELECT owner FROM playlists WHERE id = $1',
       values: [id],
     };
     const result = await this.pool.query(query);
