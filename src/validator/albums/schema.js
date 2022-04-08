5711cd3dffb07e5028ad183002626f10491c7269
ffrmns <fikrifa0@gmail.com>
2022-04-08 15:57:41 +0700

    refine year validation in albums and songs so it can't be non-integer and can't outside of between 1800 and current year

--- src/validator/albums/schema.js
+++ src/validator/albums/schema.js
@@ -1,8 +1,10 @@
 const Joi = require('joi');
 
+const currentYear = new Date().getFullYear();
+
 const AlbumPayloadSchema = Joi.object({
   name: Joi.string().required(),
-  year: Joi.number().required(),
+  year: Joi.number().integer().min(1800).max(currentYear).required(),
 });
 
 module.exports = { AlbumPayloadSchema };
--- src/validator/songs/schema.js
+++ src/validator/songs/schema.js
@@ -1,8 +1,10 @@
 const Joi = require('joi');
 
+const currentYear = new Date().getFullYear();
+
 const SongPayloadSchema = Joi.object({
   title: Joi.string().required(),
-  year: Joi.number().required(),
+  year: Joi.number().integer().min(1800).max(currentYear).required(),
   performer: Joi.string().required(),
   genre: Joi.string().required(),
   duration: Joi.number(),
